dca1949c938b29c86e2fc7daece0f1cf24c72593
voimaxis@mail.ru <voimaxis@mail.ru>
2026-05-28 20:29:51 +0300

    Fix server.log path to write to project root directory

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const logPath = path.join(__dirname, '../../server.log');
+const logPath = path.join(__dirname, '../../../server.log');
 
 export function logEvent(message: string) {
   const timestamp = new Date().toISOString();
